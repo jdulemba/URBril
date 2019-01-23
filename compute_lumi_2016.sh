b8302d57be9b309e643c805d47796e202dadc9d4
jdulemba <josdrw9@gmail.com>
2019-01-23 19:05:41 +0100

    Changed brilcalc command

--- compute_lumi_2016.sh
+++ compute_lumi_2016.sh
@@ -1,5 +1,5 @@
 
-for i in $(ls data_*.run.json); do
+for i in $(ls data_*.final_run.json); do
 		filename=$(basename "$i")
 		name="${filename%.*}"
 		name="${name%.*}"
@@ -8,12 +8,3 @@ for i in $(ls data_*.run.json); do
 		brilcalc lumi -c web --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -i $i > $name.out
 		./parse_bril_out.py $name.out > $name.lumi
 done
-
-#echo " -- PILEUP -- "
-#cmsenv
-#mergeJSON.py data_*.run.json  --output=data_all.json
-#pileupCalc.py -i data_all.json --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt --calcMode true --minBiasXsec 69000 --maxPileupBin 100 --numPileupBins 100 data.meta.pu.root
-#
-#pileupCalc.py -i data_all.json --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt --calcMode true --minBiasXsec 72450 --maxPileupBin 100 --numPileupBins 100 data.meta.pu_up.root
-#
-#pileupCalc.py -i data_all.json --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt --calcMode true --minBiasXsec 65550 --maxPileupBin 100 --numPileupBins 100 data.meta.pu_down.root
--- compute_lumi_2017.sh
+++ compute_lumi_2017.sh
@@ -5,7 +5,8 @@ for i in $(ls data_*.run.json); do
 		name="${name%.*}"
 		echo "analyzing $name"
 		#brilcalc lumi  --normtag /afs/cern.ch/user/l/lumipro/public/Normtags/normtag_DATACERT.json -i $i > $name.out
-		brilcalc lumi -i $i > $name.out
+		brilcalc lumi -c web --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -i $i > $name.out
+		#brilcalc lumi -i $i > $name.out
 		./parse_bril_out.py $name.out > $name.lumi
 
 		### find lumi for trigger during same run
@@ -14,11 +15,3 @@ for i in $(ls data_*.run.json); do
 		#./parse_bril_out.py "${trig}_${name}.out" > "${trig}_${name}.lumi"
 done
 
-#echo " -- PILEUP -- "
-##cmsenv
-#mergeJSON.py data_*.run.json  --output=data_all.json
-#pileupCalc.py -i data_all.json --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/pileup_latest.txt --calcMode true --minBiasXsec 69000 --maxPileupBin 100 --numPileupBins 100 data.meta.pu.root
-#
-#pileupCalc.py -i data_all.json --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/pileup_latest.txt --calcMode true --minBiasXsec 72450 --maxPileupBin 100 --numPileupBins 100 data.meta.pu_up.root
-#
-#pileupCalc.py -i data_all.json --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/pileup_latest.txt --calcMode true --minBiasXsec 65550 --maxPileupBin 100 --numPileupBins 100 data.meta.pu_down.root
