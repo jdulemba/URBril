935dbb0e45a563ef7aa53c2ab918a97229b843bc
jdulemba <josdrw9@gmail.com>
2019-05-09 11:49:05 +0200

    Lumi lumi files added for 2016Legacy and 2018 data+MC

---
+++ compute_lumi_2016legacy.sh
@@ -0,0 +1,10 @@
+
+for i in $(ls data_*.run.json); do
+		filename=$(basename "$i")
+		name="${filename%.*}"
+		name="${name%.*}"
+		echo "analyzing $name"
+		#brilcalc lumi  --normtag /afs/cern.ch/user/l/lumipro/public/Normtags/normtag_DATACERT.json -i $i > $name.out
+		brilcalc lumi -c web --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -i $i > $name.out
+		./parse_bril_out.py $name.out > $name.lumi
+done
